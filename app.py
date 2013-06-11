38867ad5216649910f1b220a489b51502c7a3ec2
Kyle Lahnakoski <klahnakoski@mozilla.com>
2013-06-11 19:27:30 -0400

    bug fixes, seemd to load ES now

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from util.cnv import CNV
+from util.map import Map
 from util.query import Q
-from util.maths import is_number
 from util.stats import Moments
 
 from util.db import DB
 from elasticsearch import ElasticSearch
+from util.debug import D
 from util.startup import startup
 
 
@@ -19,7 +20,7 @@ class d2e:
 
     def run(self):
         min_id=0
-        limit=1000
+        limit=100
 
         while True:
             records=self.extract(min_id, limit)
@@ -56,23 +57,32 @@ class d2e:
 
     def transform(self, records):
         for r in records:
-            r.json=CNV.JSON2object(r.json_blob)
+            r.json=CNV.JSON2object(r.json_blob).dict
             r.json_blob=None
 
-            d2e.summarize(r)
+            d2e.summarize(r.dict)
 
 
     # RESPONSIBLE FOR CONVERTING LONG ARRAYS OF NUMBERS TO THEIR REPRESENTATIVE
     # MOMENTS
     @staticmethod
     def summarize(r):
-        for k, v in r.copy():
-            try:
-                if isinstance(v, list) and all([is_number(n) for n in v]):
-                    r[k+"_moments"]=Moments.new_instance(v)
-                r[k]=None
-            finally:
-                pass
+        try:
+            for k, v in [(k,v) for k,v in r.items()]:
+                if isinstance(v, Map):
+                    d2e.summarize(v.dict)
+                elif isinstance(v, dict):
+                    d2e.summarize(v)
+                elif isinstance(v, list):
+                    try:
+                        r[k+"_moments"]=Moments.new_instance(v).tuple
+                        r[k]=None
+                    except Exception, e:
+                        for v2 in v:
+                            d2e.summarize(v2)
+        except Exception, e:
+            D.warning("Can not summarize: ${json}", {"json":CNV.object2JSON(r)})
+
 
 
 
@@ -84,10 +94,17 @@ class d2e:
 # READ SETTINGS
 settings=startup.read_settings()
 
-# BUILD SOURCE AND SINK
-dz=DB(**settings.datazilla)
-es=ElasticSearch(**settings.elasticsearch)
 
-# RUN
-converter=d2e(dz, es)
-converter.run()
+
+
+# BUILD SOURCE AND SINK
+with DB(settings.datazilla) as dz:
+    es=ElasticSearch(settings.elasticsearch)
+    with open("test_schema.json") as f:
+        schema=CNV.JSON2object(f.read(), flexible=True)
+    ElasticSearch.create_index(settings.elasticsearch, schema)
+    es.set_refresh_interval(-1)
+
+    # RUN
+    converter=d2e(dz, es)
+    converter.run()
--- elasticsearch.py
+++ elasticsearch.py
@@ -1,28 +1,85 @@
+
 import requests
-from util.map import Map
 from util.cnv import CNV
 from util.debug import D
 
+DEBUG=True
 
 class ElasticSearch():
 
-    def __init__(self, **param):
-        param=Map(**param)
-        assert param.host is not None
-        assert param.port is not None
-        assert param.index_name is not None
-        assert param.type_name is not None
-
-        self.settings=param
-        self.path=self.settings.host+":"+str(self.settings.port)+"/"+self.settings.index_name+"/"+self.settings.type_name
 
 
 
+    def __init__(self, settings):
+        assert settings.host is not None
+        assert settings.index is not None
+        assert settings.type is not None
+
+        if settings.port is None: settings.port=9200
+        
+        self.settings=settings
+        self.path=settings.host+":"+str(settings.port)+"/"+settings.index+"/"+settings.type
+
+
+
+    @staticmethod
+    def create_index(settings, schema):
+        ElasticSearch.post(
+            settings.host+":"+str(settings.port)+"/"+settings.index,
+            data=CNV.object2JSON(schema),
+            headers={"Content-Type":"application/json"}
+        )
+
+
+    @staticmethod
+    def delete_index(settings):
+        ElasticSearch.delete(
+            settings.host+":"+str(settings.port)+"/"+settings.index,
+        )
 
 
     def load(self, records):
         # ADD LINE WITH COMMAND
-        ['{"create":{"id":"'+str(r.id)+'"}}\n'+CNV.object2JSON(r) for r in records]
-        response=requests.post(self.path+"_bulk", "\n".join([CNV.object2JSON(r) for r in records]))
-        D.println(response.content)
+        lines=['{"create":{"_id":"'+str(r.id)+'"}}\n'+CNV.object2JSON(r)+"\n" for r in records]
+        response=ElasticSearch.post(
+            self.path+"/_bulk",
+            data="".join(lines),
+            headers={"Content-Type":"text"}
+        )
+        if DEBUG: D.println("${num} items added", {"num":len(CNV.JSON2object(response.content).items)})
+
+
+    # -1 FOR NO REFRESH
+    def set_refresh_interval(self, seconds):
+        ElasticSearch.put(self.path+"/_settings", data="{\"index\":{\"refresh_interval\":\""+str(seconds)+"\"}}")
+
+
         
+    @staticmethod
+    def post(*list, **args):
+        try:
+            response=requests.post(*list, **args)
+            if DEBUG: D.println(response.content[:100])
+            return response
+        except Exception, e:
+            D.error("Problem with call to ${url}", {"url":list[0]}, e)
+
+    @staticmethod
+    def put(*list, **args):
+        try:
+            response=requests.put(*list, **args)
+            if DEBUG: D.println(response.content)
+            return response
+        except Exception, e:
+            D.error("Problem with call to ${url}", {"url":list[0]}, e)
+
+    @staticmethod
+    def delete(*list, **args):
+        try:
+            response=requests.delete(*list, **args)
+            if DEBUG: D.println(response.content)
+            return response
+        except Exception, e:
+            D.error("Problem with call to ${url}", {"url":list[0]}, e)
+
+
