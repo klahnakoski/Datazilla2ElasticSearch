cfc19b3b3e9e2080013662424f55de406f569117
kyle lahnakoski <klahnakoski@mozilla.com>
2013-07-11 16:40:42 +0000

    prefix sample

--- Sample.py
+++ Sample.py
@@ -13,13 +13,16 @@ es=ElasticSearch(settings.elasticsearch)
 
 with open("Sample.tab", "w") as output_file:
     with open("raw_json_blobs.tab", "r") as input_file:
-        for line in input_file:
+        for i, line in enumerate(input_file):
             try:
                 if len(line.strip())==0: continue
                 col=line.split("\t")
                 id=int(col[0])
 #                if id!=1608289: continue
-                if 0.001<random(): continue
+
+                if i>100: break
+
+#                if 0.001<random(): continue
                 D.println(line)
 
                 data=CNV.JSON2object(col[1]).json_blob
