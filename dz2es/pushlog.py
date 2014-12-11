b0088ec9f57db7e00b0dff0f003b6e54f6137d4c
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-12-11 12:09:33 -0500

    do not even try to find revision=="NULL"

--- dz2es/pushlog.py
+++ dz2es/pushlog.py
@@ -1,3 +1,14 @@
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+from __future__ import unicode_literals
+
 import requests
 from dz2es.mozilla_graph import MozillaGraph
 from dz2es.repos.revisions import Revision
@@ -20,6 +31,9 @@ class Pushlog(object):
         try:
             if not isinstance(item, (tuple, list)):
                 Log.error("Expecting a [branch, rev] pair")
+
+            if item[1] == "NULL":
+                return None
 
             push = self.graph.get_push(Revision(
                 branch=self.branches[talos2treeherder(item[0])],
